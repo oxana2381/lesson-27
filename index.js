f68d4124c4adb7b140e46300bed8606b2cabd7b2
Oxana Ciobanu <oxanochka23@yahoo.com>
2020-06-10 13:11:05 +0300

    add apod

--- index.js
+++ index.js
@@ -1,64 +1,93 @@
+
+
 function Apod(id){
     this.id = id;
     this.date = undefined;
     let body = document.body;
     this.container = document.createElement('div');
     this.container.setAttribute('id', this.id);
+    this.dateFormatter = dateFormatter;
+
+    this. renderApp = function(data) {
+        let title = document.createElement('h1');
+        title.innerText = data.title;
+        
+    
+        let imageDiv = document.createElement('div');
+        imageDiv.classList.add('image');
+    
+       
+        if(data.media_type === 'video') {
+            let iframe = document.createElement('iframe');
+            iframe.setAttribute('src', data.url);
+            iframe.setAttribute('width', '450');
+            iframe.setAttribute('height', '300');
+            imageDiv.append(iframe);
+    
+        } else {
+            let image = document.createElement('img');
+            image.setAttribute('src', data.url);
+            image.setAttribute('width', '450');
+            image.setAttribute('height', '300');
+            imageDiv.append(image);
+        }
+        let paragraph = document.createElement('p');
+        paragraph.innerText = data.explanation;
+    
+        if(data.copyright) {
+            let copyrightDiv = document.createElement('div');
+            copyrightDiv.classList.add('copyright');
+        
+            let copyrightSpan = document.createElement('span');
+            copyrightSpan.innerHTML = '© ' + data.copyright;
+            copyrightDiv.append(copyrightSpan);
+            this.container.append(copyrightDiv);
+        }
+        
+        this.container.append(title);
+        this.container.append(imageDiv);
+        this.container.append(paragraph);
+    }
+        
+       
+
 
     this.addPictureOfDay  = function() {
         let element = document.createElement('div');
-        if(this.date === undefined) {
-          
-        
-           this.date=(new Date().getFullYear() +'-'+''+( new Date().getMonth()+1)+'-'+ ''+( new Date().getDate()))
-           
-          
-        } else {
-           // this.date =new Date(this.date.setDate((new Date().getFullYear() +'-'+''+( new Date().getMonth()+1)+'-'+ ''+( new Date().getDate()-1))))
-           
-          
-        }
-        
-        fetch('https://api.nasa.gov/planetary/apod?api_key=AgAOKPwUaNaoMb7jMr4ETKQGGqlMtzRNGwSVaHNf&date='+this.date)
-    .then(response => response.json())
-    .then(data => renderApp(data))
 
-function renderApp(data) {
-    let body = document.body;
-    let title = document.createElement('h1');
-    title.innerText = data.title;
-    
-
-    let imageDiv = document.createElement('div');
-    
-    imageDiv.classList.add('image');
-
-    if(data.media_type === 'video') {
-        let iframe = document.createElement('iframe');
-        iframe.setAttribute('src', data.url);
-        iframe.setAttribute('width', '450');
-        iframe.setAttribute('height', '300');
-        imageDiv.append(iframe);
-
+      if(this.date===undefined ) {
+       this.date = new Date;
     } else {
-        let image = document.createElement('img');
-        image.setAttribute('src', data.url);
-        image.setAttribute('width', '450');
-        image.setAttribute('height', '300');
-        imageDiv.append(image);
-    }
-    
-    
-    body.append(title);
-    body.append(imageDiv);
+        this.date = new Date(this.date.setDate(this.date.getDate() -1))
+       
+    };
+          
+        
+        
+        
+    fetch('https://api.nasa.gov/planetary/apod?api_key=AgAOKPwUaNaoMb7jMr4ETKQGGqlMtzRNGwSVaHNf&date=' + this.dateFormatter())
+    .then(response => response.json())
+    .then(data => this.renderApp(data))
   
-}
 
-        element.innerText = this.date;
+   
+
+
+        element.innerText = this.dateFormatter();
         this.container.append(element)
     }
     this.addPictureOfDay ();
     body.append(this.container);
 }
 
-const apod = new Apod('apod');
+let apod = new Apod('apod');
+
+function dateFormatter () {
+    var now = this.date;
+    var y = now.getFullYear();
+    var m = now.getMonth() + 1;
+    var d = now.getDate();
+    var mm = m < 10 ? '0' + m : m;
+    var dd = d < 10 ? '0' + d : d;
+    return '' + y + '-' + mm + '-' + dd;
+}
